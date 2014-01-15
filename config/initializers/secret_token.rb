db2ad2b37af2025d976871a047606a6a0ec8c0ec
Andre Mroz <mathmonkey4@gmail.com>
2014-01-15 16:58:22 -0500

    adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlyDelts::Application.config.secret_key_base = 'bb0e6b1d35cd30cd275b86aaf26c2c7ff79f394b3bf16146b9abdb2a9a9d3c7a324d3f29bcef9f00543c07bb7c1095222f19e4e0288ac20379ee84f343f82536'
